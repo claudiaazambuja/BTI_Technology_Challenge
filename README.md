b3f98b79566ccf068289f7d8d8a568e15c1e342c
cclabdis <cclabdis@gmail.com>
2023-12-01 20:50:27 -0300

    chore(database): Update SQL scripts and adjust README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BTI_Technology_Challenge
 Welcome to the BTI Technology Coding Challenge repository! 🚀 This challenge is designed to assess  programming skills in JavaScript and database design abilities.
+
+
+Deploy:
+[FREESHOP-Calculator](https://bti-technology-challenge.vercel.app/)
+
+[tollEase](https://bti-technology-challenge-xabf.vercel.app/)
---
+++ answers/Question_2/answer_question2_toll.sql
@@ -0,0 +1,45 @@
+-- Table 
+CREATE TABLE TollBooth (
+    id SERIAL PRIMARY KEY,
+    vehicle_id INT,
+    passage_date DATE,
+    passage_fee DECIMAL(5,2),
+    accumulated_passages INT,
+    discount_applied BOOLEAN
+);
+
+
+-- Command to insert 
+INSERT INTO TollBooth (vehicle_id, passage_date, passage_fee, accumulated_passages, discount_applied) 
+VALUES 
+    (1, '2023-12-01', 7.90, 1, FALSE),
+    (1, '2023-12-02', 7.90, 2, FALSE),
+    -- ... (inserir mais dados conforme necessário)
+    (1, '2023-12-11', 7.90, 11,  TRUE),
+    (1, '2023-12-16', 7.90, 16, TRUE),
+    (1, '2023-12-21', 7.90, 21, TRUE),
+    (1, '2023-12-30', 7.90, 30, TRUE);
+
+-- Comando para verificar as passagens de um veículo
+SELECT *
+FROM TollBooth
+WHERE vehicle_id = 1;
+
+-- Atualização para garantir que o desconto aplicado não ultrapasse 20% ou o valor mínimo de 1,58 (o que for maior)
+-- UPDATE TollBooth
+-- SET
+--     accumulated_discount = 
+--         CASE
+--             WHEN accumulated_passages >= 10 AND discount_applied = FALSE THEN
+--                 LEAST(passage_fee * 0.20, accumulated_passages / 10 * 0.05 * passage_fee)
+--             ELSE
+--                 accumulated_discount
+--         END,
+--     discount_applied = TRUE
+-- WHERE accumulated_passages >= 10 AND discount_applied = FALSE;
+
+
+URL = postgres://barxvapl:ryZ-pu5OnhVXr5hRgc_85nuizQjwDDRH@bubble.db.elephantsql.com/barxvapl
+
+
+
