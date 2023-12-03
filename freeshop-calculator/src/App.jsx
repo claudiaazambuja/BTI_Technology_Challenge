9bf1efc9de9ce7a9a21f984fc38a3ee3d2177b84
cclabdis <cclabdis@gmail.com>
2023-12-03 20:57:17 -0300

    chore(folder-structure): Update folder structures

--- freeshop-calculator/src/App.jsx
+++ freeshop-calculator/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { HashRouter as Router, Route, Switch} from 'react-router-dom';
+
+// import Block1View from './Block1View'; // Importe suas views
+// import Block2View from './Block2View';
+// import Block3View from './Block3View';
+import ErrorPage from './pages/ErrorPage.jsx'; // Crie uma página de erro
+
+
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+const VITE_API_KEY = import.meta.env.VITE_API_KEY;
+
+
+console.log('Rendering App component');
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Block1View} />
+        <Route path="/block2" component={Block2View} />
+        <Route path="/block3" component={Block3View} />
+        {/* Redireciona qualquer outra rota para a página de erro */}
+        <Redirect from="*" to="/error" component={ErrorPage} />
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+
+
+
+
+export default App;
