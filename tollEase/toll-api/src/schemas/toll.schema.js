fd4fc6dc49509659acb651f60d475799abdf61ca
cclabdis <cclabdis@gmail.com>
2023-12-04 19:19:47 -0300

    chore(schema): Update schema to handle two car plate formats

--- tollEase/toll-api/src/schemas/toll.schema.js
+++ tollEase/toll-api/src/schemas/toll.schema.js
@@ -4,11 +4,11 @@ const joi = joiBase.extend(joiDate);
 
 export const plaqueSchema = joi.object({
     plaque: joi.string()
-        .pattern(/^[A-Z]{3}-\d{4}$/) // Assumindo um formato padrão AAA-1234 | AAA-1A11, ajuste conforme necessário
+        .pattern(/^[A-Za-z]{3}-\d{1}[A-Za-z]?\d{2,3}$/) // Aceita tanto letras maiúsculas quanto minúsculas
         .required()
         .messages({
             'string.base': `"Número de placa" deve ser uma string.`,
-            'string.pattern.base': `O formato da placa deve ser AAA-1234.`,
+            'string.pattern.base': `O formato da placa deve ser AAA-1234 ou AAA-1a23.`,
             'any.required': `O campo "Número de placa" é obrigatório.`
         })
 });
