aeb5c24ffefc05b157faaf34370c4906353ba78e
cclabdis <cclabdis@gmail.com>
2023-12-04 09:58:19 -0300

    chore(favicon): Add new favicon

--- freeshop-calculator/src/App.jsx
+++ freeshop-calculator/src/App.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch} from 'react-router-dom';
-
-// import Block1View from './Block1View'; // Importe suas views
-// import Block2View from './Block2View';
-// import Block3View from './Block3View';
-import ErrorPage from './pages/ErrorPage.jsx'; // Crie uma página de erro
-
-
-const VITE_API_URL = import.meta.env.VITE_API_URL;
-const VITE_API_KEY = import.meta.env.VITE_API_KEY;
-
+import { HashRouter as Router, Route} from 'react-router-dom';
 
 console.log('Rendering App component');
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Block1View} />
-        <Route path="/block2" component={Block2View} />
-        <Route path="/block3" component={Block3View} />
-        {/* Redireciona qualquer outra rota para a página de erro */}
-        <Redirect from="*" to="/error" component={ErrorPage} />
-      </Switch>
+      <Route exact path="/" component={Block1View} />
     </Router>
   );
 };
