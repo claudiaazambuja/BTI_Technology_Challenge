b99563083c4cdf84deff8b25da6c166db934fb69
cclabdis <cclabdis@gmail.com>
2023-12-04 02:57:24 -0300

    update(README) toll api

--- tollEase/toll-api/README.md
+++ tollEase/toll-api/README.md
@@ -1,3 +1,5 @@
 
 
+[Deploy Backend](https://bti-toll-api.onrender.com)
+
 Para rodar a seed: psql -U seu_usuario -d seu_banco_de_dados -a -f seed.sql
--- tollEase/toll-front/src/App.jsx
+++ tollEase/toll-front/src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import RefreshProvider from "./contexts/refreshContext";
 import React from 'react';
 import MainBox from './pages/MainBox/MainBox.jsx';
 
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <MainBox />
-    </div>
+    <BrowserRouter>
+      <RefreshProvider>
+        <Routes>
+          <Route path="/" element={<MainBox />} />
+          <Route path="/*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </RefreshProvider>
+    </BrowserRouter>
   );
 };
 
